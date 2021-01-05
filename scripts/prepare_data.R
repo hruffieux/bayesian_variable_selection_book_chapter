5ed578c60999126b9015c7f1945485aa02874413
Helene Ruffieux <helene.ruffieux@alumni.epfl.ch>
2021-01-05 15:28:05 +0100

    Added Rmd file.

--- scripts/prepare_data.R
+++ scripts/prepare_data.R
@@ -8,7 +8,7 @@ main_dir <- file.path(CORE_DIR, "bayesian_variable_selection_book_chapter/script
 setwd(main_dir)
 
 RNGkind("L'Ecuyer-CMRG") # to ensure reproducibility when using parallel processes
-my_seed <- 123
+my_seed <- 1
 set.seed(my_seed)
 
 ## -----------------------------------------------------------------------------
@@ -32,37 +32,50 @@ load(file.path(data_dir, "replicated_data.RData"))
 
 vec_type <- names(list_expr)
 
+p <- unique(sapply(list_repl_snps, ncol))
+stopifnot(length(p) == 1) # same number of of SNPs for all conditions
+
+q <- unique(sapply(list_expr, ncol))
+stopifnot(length(q) == 1) # same number of of levels for all conditions
+
+## simulates association pattern between the SNPs and the expression levels
+# same active SNPs and active levels for all conditions
+# but a given active SNP may be associated with different active levels across the conditions
+#
+p0 <- 5  # number of active SNPs
+ind_p0 <- sort(sample(1:p, p0, replace = FALSE))
+
+q0 <- 250  # number of expression levels under genetic control
+ind_q0 <- sort(sample(1:q, q0, replace = FALSE))
+
+# vec_prob_sh: vector of length p0 providing the probabilities with which each
+# active SNP will be associated with an additional active expression level
+vec_prob_sh <- rbeta(p0, shape1 = 1, shape2 = 2)
+
+max_tot_pve <- 0.5 # maximum total proportion of outcome variance explained by the SNPs.
+
 list_data <- NULL
 for (type in vec_type) {
 
   replicated_snps <- list_repl_snps[[type]]
   original_expr <- list_expr[[type]]
 
+  n <- unique(c(nrow(replicated_snps), nrow(original_expr)))
+  stopifnot(length(n) == 1) # same number of samples for SNPs and expression levels
+  
   rownames(original_expr) <- rownames(replicated_snps)
-
-  n <- nrow(replicated_snps)
-  p <- ncol(replicated_snps)
-  d <- ncol(original_expr)
 
   obj_snps <- convert_snps(replicated_snps)
   obj_expr <- convert_phenos(original_expr)
 
-  ## simulates association pattern between the SNPs and the expression levels
-  #
-  p0 <- 10  # number of active SNPs
-  ind_p0 <- sample(1:p, p0, replace = FALSE)
-
-  d0 <- 250  # number of expression levels under genetic control
-  ind_d0 <- sample(1:d, d0, replace = FALSE)
-
-  # vec_prob_sh: vector of length p0 providing the probabilities with which each
-  # active SNP will be associated with an additional active expression level
-  vec_prob_sh <- rbeta(p0, shape1 = 1, shape2 = 2)
-
-  max_tot_pve <- 0.5 # maximum total proportion of outcome variance explained by the SNPs.
-
-  obj_data <- generate_dependence(obj_snps, obj_expr, ind_d0, ind_p0,
-                                  vec_prob_sh, family = "gaussian",
+  # add some noise so these propensities differ slightly depending on the condition
+  vec_prob_sh_type <- vec_prob_sh + rnorm(p0, sd = 0.1)
+  
+  # make sure that between zero and one
+  vec_prob_sh_type[vec_prob_sh_type < .Machine$double.eps | vec_prob_sh_type > 1 - .Machine$double.eps] <- vec_prob_sh[vec_prob_sh_type < .Machine$double.eps | vec_prob_sh_type > 1 - .Machine$double.eps]
+  
+  obj_data <- generate_dependence(obj_snps, obj_expr, ind_q0, ind_p0,
+                                  vec_prob_sh_type, family = "gaussian",
                                   max_tot_pve = max_tot_pve)
 
   stopifnot(all.equal(rownames(obj_data$snps), rownames(obj_data$phenos)))
